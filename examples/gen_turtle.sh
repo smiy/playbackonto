a569518464d5a6799f09ca2cd90c5a90bd524490
Bo Ferri <zazi@smiy.org>
2010-07-21 11:17:45 +0000

    playbackonto:  * ADDED: playlist example

---
+++ examples/gen_turtle.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+
+# Generate the Turtle from the RDF/XML code
+
+rapper -i rdfxml -o turtle RDF/playlist_-_example.rdf > N3/playlist_-_example.n3
---
+++ examples/gen_xml.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+
+# Generate the RDF/XML from the Turtle code
+
+rapper -i turtle -o rdfxml-abbrev N3/playlist_-_example.n3 > RDF/playlist_-_example.rdf
