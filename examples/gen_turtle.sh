0cc73de57a223fcfc945102a75abf4c853f6dd67
Bo Ferri <zazi@smiy.org>
2010-07-23 19:23:17 +0000

    playbackonto:  * UPDATED: playlist graphic  * ADDED: play back and skip counter example

--- examples/gen_turtle.sh
+++ examples/gen_turtle.sh
@@ -2,4 +2,5 @@
 
 # Generate the Turtle from the RDF/XML code
 
+rapper -i rdfxml -o turtle RDF/playback_and_skip_counter_-_example.rdf > N3/playback_and_skip_counter_-_example.n3
 rapper -i rdfxml -o turtle RDF/playlist_-_example.rdf > N3/playlist_-_example.n3
--- examples/gen_xml.sh
+++ examples/gen_xml.sh
@@ -3,3 +3,4 @@
 # Generate the RDF/XML from the Turtle code
 
 rapper -i turtle -o rdfxml-abbrev N3/playlist_-_example.n3 > RDF/playlist_-_example.rdf
+rapper -i turtle -o rdfxml-abbrev N3/playback_and_skip_counter_-_example.n3 > RDF/playback_and_skip_counter_-_example.rdf
