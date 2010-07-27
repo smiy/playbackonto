58121bca3836646f9ace6e03a7f00762866f3b60
Bo Ferri <zazi@smiy.org>
2010-07-27 18:01:07 +0000

    playbackonto:  * FIXED: minor changes

--- examples/gen_turtle.sh
+++ examples/gen_turtle.sh
@@ -4,3 +4,4 @@
 
 rapper -i rdfxml -o turtle RDF/playback_and_skip_counter_-_example.rdf > N3/playback_and_skip_counter_-_example.n3
 rapper -i rdfxml -o turtle RDF/playlist_-_example.rdf > N3/playlist_-_example.n3
+rapper -i rdfxml -o turtle RDF/skip_counter_-_example.rdf > N3/skip_counter_-_example.n3
